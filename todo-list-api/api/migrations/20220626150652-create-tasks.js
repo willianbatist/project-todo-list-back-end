e5882ba5c298fe91dafc5848fa64554d91d6a61c
Willian <willian.alves.b15@gmail.com>
2022-06-26 13:12:46 -0300

    feat: criando a migration de tasks

---
+++ todo-list-api/api/migrations/20220626150652-create-tasks.js
@@ -0,0 +1,29 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      created_date: { 
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+     },
+     status: {
+        allowNull: false,
+        type: Sequelize.STRING,
+     },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
