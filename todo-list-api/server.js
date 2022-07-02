c2966f16e16123d49ad0144f398c8e762349d1da
Willian <willian.alves.b15@gmail.com>
2022-07-01 21:19:00 -0300

    fix: renomendo variavel de ambiente no server.js

--- todo-list-api/server.js
+++ todo-list-api/server.js
@@ -1,7 +1,7 @@
 require('dotenv/config');
 const app = require('./api/api.js');
 
-const port = process.env.NODE_DOCKER_PORT;
+const port = process.env.PORT;
 
 app.get('/', (_request, response) => {
   response.json({ message: "Welcome to todo-list application." })
