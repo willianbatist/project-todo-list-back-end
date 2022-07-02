0f90dcdf3f8743990f5cbe7d0bbbd152d8ac16bf
Willian <willian.alves.b15@gmail.com>
2022-07-01 21:17:07 -0300

    fix: renomeando variaveis de ambiente do config.js

--- todo-list-api/api/config/config.js
+++ todo-list-api/api/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    host: "192.168.48.1",
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false,
   },
