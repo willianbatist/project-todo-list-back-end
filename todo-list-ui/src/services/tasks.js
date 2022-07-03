dfc91b33e38abe7571c4ef8a7bd24ca9a5852338
Willian <willian.alves.b15@gmail.com>
2022-07-03 18:04:24 -0300

    feat: criando service delete em front-end

--- todo-list-ui/src/services/tasks.js
+++ todo-list-ui/src/services/tasks.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
-import { GETTASKS, POSTTASK } from '../constants/index';
+import { GETTASKS, POSTTASK, DELETETASK } from '../constants/index';
 
-function getTasks(setStates) {
+const getTasks = (setStates) => {
   return axios.get(GETTASKS).then((res) => {
     setStates(res.data);
   });
 }
 
-function postTask(states) {
+const postTask = (states) => {
   axios.post(POSTTASK, {
     task: states.task,
     status: states.status,
   }).then((res) => console.log(res));
 }
 
+const deleteTask = (task) => {
+  axios.delete(DELETETASK, {
+    task
+  }).then((res) => console.log(res));
+}
+
 export {
   getTasks,
   postTask,
+  deleteTask,
 }
